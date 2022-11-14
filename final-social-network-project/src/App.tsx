bd9550dce95f92ea695f6179e6034ca68bc04ed1
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-11-14 22:28:00 +0100

    start dev final SoshialNetwork

---
+++ final-social-network-project/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+
+    return (
+        <div className="App">
+
+            <ul>
+                <li>JS</li>
+                <li>TS</li>
+                <li>REACT</li>
+                <li>REDUX</li>
+                <li>CSS</li>
+                <li>HTML</li>
+            </ul>
+        </div>
+    );
+}
+
+export default App;
