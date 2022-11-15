29cddd095051b606f70c457292895759db7efa28
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-11-15 22:08:31 +0100

    update SN #12

--- final-social-network-project/src/App.tsx
+++ final-social-network-project/src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Footer } from './Footer';
-import {Header} from './Heder';
-import {Technologies} from './Technologies';
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 const App = () => {
 
     return (
-        <div>
+        <div className='app-wrapper'>
             <Header/>
-            <Technologies/>
-            <Footer/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
