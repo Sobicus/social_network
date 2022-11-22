a54f2bc04c2d84f46cd725814dfab6d316704d27
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-11-22 20:16:21 +0100

    Update SocialNetwork v21.0

--- final-social-network-project/src/components/Dialogs/Dialogs.tsx
+++ final-social-network-project/src/components/Dialogs/Dialogs.tsx
@@ -1,7 +1,21 @@
+import style from './Dialogs.module.css'
+
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogs_items}>
+                <div className={style.dialog + ' ' + style.active}>Max</div>
+                <div className={style.dialog}>Alina</div>
+                <div className={style.dialog}>Vika</div>
+                <div className={style.dialog}>Lubov</div>
+                <div className={style.dialog}>Anatoliy</div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How are you?</div>
+                <div className={style.message}>Good, and you?</div>
+            </div>
+
         </div>
     )
 }
