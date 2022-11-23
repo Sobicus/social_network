6d68339b3cb1b340386dc0072b2b57b731a6e770
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-11-23 15:34:46 +0100

    updated SocialNetwork v24.0

--- final-social-network-project/src/components/Dialogs/Dialogs.tsx
+++ final-social-network-project/src/components/Dialogs/Dialogs.tsx
@@ -25,9 +25,31 @@ const Message = (props: MessagePropsType) => {
     )
 }
 export const Dialogs = () => {
+
+    type dialogsDataType = {
+        id: string
+        name: string
+    }
+    const dialogsData: Array<dialogsDataType> = [
+        {id: '1', name: 'Max'},
+        {id: '2', name: 'Alina'},
+        {id: '3', name: 'Vika'},
+        {id: '4', name: 'Lubov'},
+        {id: '5', name: 'Anatoliy'},
+    ]
+    type messageDataType = {
+        id: string
+        message: string
+    }
+    const messageData: Array<messageDataType> = [
+        {id: '1', message: 'Hi'},
+        {id: '1', message: 'How are you?'},
+        {id: '1', message: 'Good, and you?'},
+    ]
     return (
         <div className={style.dialogs}>
             <div className={style.dialogs_items}>
+                {dialogsData.map(el => <DialogItem id={el.id} name={el.name} key={el.id}/>)}
                 <DialogItem id='1' name='Max'/>
                 <DialogItem id='2' name='Alina'/>
                 <DialogItem id='3' name='Vika'/>
@@ -35,8 +57,9 @@ export const Dialogs = () => {
                 <DialogItem id='5' name='Anatoliy'/>
             </div>
             <div className={style.messages}>
+                {messageData.map(el => <Message message={el.message} key={el.id}/>)}
                 <Message message='Hi'/>
-                <Message message='How are you?\n'/>
+                <Message message='How are you?'/>
                 <Message message='Good, and you?'/>
             </div>
 
--- final-social-network-project/src/components/Profile/MyPosts/MyPosts.tsx
+++ final-social-network-project/src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,20 @@ import style from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
+
+    type postDataType = {
+        id: string
+        message: string
+        likesCounter: number
+    }
+
+    const postData: Array<postDataType> = [
+        {id: '1', message: 'It`s my first post', likesCounter: 1},
+        {id: '2', message: 'Hi, how are you?', likesCounter: 5},
+        {id: '3', message: 'GO GO GO?', likesCounter: 7},
+        {id: '4', message: 'Test props', likesCounter: 13},
+    ]
+
     return (
         <div className={style.posts_block}>
             <h3>
@@ -17,6 +31,7 @@ export const MyPosts = () => {
                 </div>
             </div>
             <div className={style.posts}>
+                {postData.map(el=><Post message={el.message} likesCounter={el.likesCounter} key={el.id}/>)}
                 <Post message='It`s my first post' likesCounter={1}/>
                 <Post message='Hi, how are you?' likesCounter={5}/>
                 <Post message='GO GO GO?' likesCounter={7}/>
