b9c3c5f4a6051dd7cfe3962113dde548c24b4e0c
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-12-06 21:51:37 +0100

    update SocialNetwork  v37.0

--- final-social-network-project/src/redux/state.ts
+++ final-social-network-project/src/redux/state.ts
@@ -1,64 +1,129 @@
-let rerenderEntireTree = (state: stateType) => {
-    console.log('State changed')
-}
-export const addNewPost = () => {
-    const newPost: postsDataType = {id: '5', message: state.profilePage.newPostText, likesCounter: 1}
-    state.profilePage.postsData.push(newPost)
-    state.profilePage.newPostText = ''
-    rerenderEntireTree(state)
-}
-export const updateNewPostText = (newText: string) => {
-    state.profilePage.newPostText = newText
-    rerenderEntireTree(state)
-}
-export const subscribe = (observer: (state: stateType) => void) => {
-    rerenderEntireTree = observer
-}
-export const state: stateType = {
-    profilePage: {
-        postsData: [
-            {id: '1', message: 'It`s my first post', likesCounter: 1},
-            {id: '2', message: 'Hi, how are you?', likesCounter: 5},
-            {id: '3', message: 'GO GO GO?', likesCounter: 7},
-            {id: '4', message: 'Test props', likesCounter: 13},
-        ],
-        newPostText: ''
+export let store: storeType = {
+    _state: {
+        profilePage: {
+            postsData: [
+                {id: '1', message: 'It`s my first post', likesCounter: 1},
+                {id: '2', message: 'Hi, how are you?', likesCounter: 5},
+                {id: '3', message: 'GO GO GO?', likesCounter: 7},
+                {id: '4', message: 'Test props', likesCounter: 13},
+            ],
+            newPostText: ''
+        },
+        messagesPage: {
+            dialogsData: [
+                {
+                    id: '1',
+                    name: 'Max',
+                    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
+                },
+                {
+                    id: '2',
+                    name: 'Alina',
+                    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
+                },
+                {
+                    id: '3',
+                    name: 'Vika',
+                    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
+                },
+                {
+                    id: '4',
+                    name: 'Lubov',
+                    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
+                },
+                {
+                    id: '5',
+                    name: 'Anatoliy',
+                    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
+                },
+            ],
+            messageData: [
+                {id: '1', message: 'Hi'},
+                {id: '2', message: 'How are you?'},
+                {id: '3', message: 'Good, and you?'},
+            ]
+        },
     },
-    messagesPage: {
-        dialogsData: [
-            {
-                id: '1',
-                name: 'Max',
-                avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
-            },
-            {
-                id: '2',
-                name: 'Alina',
-                avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
-            },
-            {
-                id: '3',
-                name: 'Vika',
-                avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
-            },
-            {
-                id: '4',
-                name: 'Lubov',
-                avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
-            },
-            {
-                id: '5',
-                name: 'Anatoliy',
-                avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
-            },
-        ],
-        messageData: [
-            {id: '1', message: 'Hi'},
-            {id: '2', message: 'How are you?'},
-            {id: '3', message: 'Good, and you?'},
-        ]
+    rerenderEntireTree(state: stateType) {
+        console.log('State changed')
     },
+    addNewPost() {
+        const newPost: postsDataType = {id: '5', message: state.profilePage.newPostText, likesCounter: 1}
+        state.profilePage.postsData.push(newPost)
+        state.profilePage.newPostText = ''
+        rerenderEntireTree(state)
+    },
+    updateNewPostText = (newText: string) => {
+        state.profilePage.newPostText = newText
+        rerenderEntireTree(state)
+    },
+    subscribe(observer: (state: stateType) => void) {
+        rerenderEntireTree = observer
+    }
 }
+
+// let rerenderEntireTree = (state: stateType) => {
+//     console.log('State changed')
+// }
+// export const addNewPost = () => {
+//     const newPost: postsDataType = {id: '5', message: state.profilePage.newPostText, likesCounter: 1}
+//     state.profilePage.postsData.push(newPost)
+//     state.profilePage.newPostText = ''
+//     rerenderEntireTree(state)
+// }
+// export const updateNewPostText = (newText: string) => {
+//     state.profilePage.newPostText = newText
+//     rerenderEntireTree(state)
+// }
+// export const subscribe = (observer: (state: stateType) => void) => {
+//     rerenderEntireTree = observer
+// }
+// export const state: stateType = {
+//     profilePage: {
+//         postsData: [
+//             {id: '1', message: 'It`s my first post', likesCounter: 1},
+//             {id: '2', message: 'Hi, how are you?', likesCounter: 5},
+//             {id: '3', message: 'GO GO GO?', likesCounter: 7},
+//             {id: '4', message: 'Test props', likesCounter: 13},
+//         ],
+//         newPostText: ''
+//     },
+//     messagesPage: {
+//         dialogsData: [
+//             {
+//                 id: '1',
+//                 name: 'Max',
+//                 avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
+//             },
+//             {
+//                 id: '2',
+//                 name: 'Alina',
+//                 avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
+//             },
+//             {
+//                 id: '3',
+//                 name: 'Vika',
+//                 avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
+//             },
+//             {
+//                 id: '4',
+//                 name: 'Lubov',
+//                 avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
+//             },
+//             {
+//                 id: '5',
+//                 name: 'Anatoliy',
+//                 avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhBStQk5WBxo9GGOnZ8vLmViRGavDshthEnw&usqp=CAU"
+//             },
+//         ],
+//         messageData: [
+//             {id: '1', message: 'Hi'},
+//             {id: '2', message: 'How are you?'},
+//             {id: '3', message: 'Good, and you?'},
+//         ]
+//     },
+// }
+
 export type stateType = {
     profilePage: profilePageType
     messagesPage: messagesPageType
@@ -84,4 +149,12 @@ export type postsDataType = {
     id: string
     message: string
     likesCounter: number
+}
+
+
+export type storeType = {
+    _state:stateType
+    rerenderEntireTree: (state: stateType)=>void
+    addNewPost:()=>void
+    updateNewPostText : (newText: string) => void
 }
