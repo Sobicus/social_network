86be0eeddfb6b282da298dabd7c55d17b8b11bbe
Sobicus <83043123+Sobicus@users.noreply.github.com>
2023-01-04 23:46:17 +0100

    Update SocialNetwork v45.1

--- final-social-network-project/src/components/Dialogs/DialogsContainer.tsx
+++ final-social-network-project/src/components/Dialogs/DialogsContainer.tsx
@@ -1,31 +1,45 @@
-import style from './Dialogs.module.css'
-import {DialogItem} from "./DialogItem/DialogsItem";
-import {Message} from "./Message/Message";
-import {storeType} from '../../redux/store';
-import React, {ChangeEvent} from "react";
+import React from "react";
+import {stateType} from '../../redux/store';
 import {sendMessageAC, updateNewMessageBodyAC} from '../../redux/dialogs-reducer';
 import {Dialogs} from "./Dialogs";
-import {StoreContext} from "../../StoreContext";
+import {connect} from "react-redux";
 
-type DialogsContainerType = {
-    store: storeType
+// type DialogsContainerType = {
+//     store: storeType
+// }
+
+// export const DialogsContainer: React.FC<DialogsContainerType> = (props) => {
+//
+//     return <StoreContext.Consumer>
+//         {(store) => {
+//             let state = store.getState().messagesPage
+//
+//             const onSendMessageClick = () => {
+//                 store.dispatch(sendMessageAC())
+//             }
+//             const onNewMessageChange = (body: string) => {
+//                 store.dispatch(updateNewMessageBodyAC(body))
+//             }
+//             return <Dialogs
+//                 updateNewMessageBody={onNewMessageChange}
+//                 sendMessage={onSendMessageClick}
+//                 messagesPage={store.getState().messagesPage}
+//             />
+//         }
+//         }
+//     </StoreContext.Consumer>
+// }
+
+let mapStateeToProps = (state: stateType) => {
+    return {
+        messagesPage: state.messagesPage
+    }
+}
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewMessageBody:(body: string)=>{dispatch(updateNewMessageBodyAC(body))},
+        sendMessage:()=>{ dispatch(sendMessageAC())}
+    }
 }
 
-export const DialogsContainer: React.FC<DialogsContainerType> = (props) => {
-
-    return <StoreContext.Consumer>
-        {(store) => {
-            let state = store.getState().messagesPage
-
-            const onSendMessageClick = () => {
-                store.dispatch(sendMessageAC())
-            }
-            const onNewMessageChange = (body: string) => {
-                store.dispatch(updateNewMessageBodyAC(body))
-            }
-            return <Dialogs updateNewMessageBody={onNewMessageChange} sendMessage={onSendMessageClick} messagesPage={state}/>
-        }
-        }
-    </StoreContext.Consumer>
-}
-
+const DialogsContainer = connect(mapStateeToProps, mapDispatchToProps)(Dialogs)
--- final-social-network-project/src/index.tsx
+++ final-social-network-project/src/index.tsx
@@ -7,7 +7,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import {stateType} from './redux/store'
-import {Provider} from "./StoreContext";
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
