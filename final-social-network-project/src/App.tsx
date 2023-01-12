93eac676d8b4937f7ecab5a3af11b672c9b07f3a
Sobicus <83043123+Sobicus@users.noreply.github.com>
2023-01-12 20:36:35 +0100

    update SocialNetwork v 49.0

--- final-social-network-project/src/App.tsx
+++ final-social-network-project/src/App.tsx
@@ -8,6 +8,7 @@ import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
 import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
+import {Users} from "./components/Users/Users";
 
 type AppType = {}
 
@@ -18,12 +19,13 @@ const App: React.FC<AppType> = (props) => {
             <Navbar/>
             <div className='app_wrapper_content'>
                 <Routes>
-                    <Route path='/profile' element={<Profile/>}
+                    <Route path='/profile/*' element={<Profile/>}
                     />
                     <Route
-                        path='/dialogs/*'
+                        path='/dialogs/'
                         element={<DialogsContainer/>}
                     />
+                    <Route path='/users' element={<Users/>}/>
                     <Route path='/news' element={<News/>}/>
                     <Route path='/music' element={<Music/>}/>
                     <Route path='/settings' element={<Settings/>}/>
---
+++ final-social-network-project/src/redux/users-reducer.ts
@@ -0,0 +1,10 @@
+let initialState={
+
+}
+export const usersReducer = (state: any = initialState, action: any) => {
+    switch (action.type) {
+        default:
+            return state
+
+    }
+}
